b1592148bebbd9aa8c9e37f620f54895a9ced64d
Peter Mwaura <kimmwaus@gmail.com>
2024-09-17 23:43:27 +0300

    update to the users section

--- app/Http/Controllers/MarketingController.php
+++ app/Http/Controllers/MarketingController.php
@@ -7,8 +7,63 @@ use Inertia\Inertia;
 
 class MarketingController extends Controller
 {
-    public function index(Request $request, $page = 1)
+    public function index(Request $request, $page = 'premier_league')
     {
-        return Inertia::render('Marketing/ListDesign');
+        return Inertia::render('Marketing/ListDesign', [
+                'events' => $this->data_resource($page)
+            ]
+        );
+    }
+
+    public function data_resource($page)
+    {
+        switch ($page) {
+            case 'premier_league':
+                return [
+                    [
+                        "home" => "https://apiv3.apifootball.com/badges/3072_southampton.jpg",
+                        "away" => "https://apiv3.apifootball.com/badges/102_manchester-united.jpg",
+                        "prediction" => "2",
+                        "status" => "completed",
+                        "home_score" => "0",
+                        "away_score" => "3",
+                    ],
+                    [
+                        "home" => "https://apiv3.apifootball.com/badges/80_manchester-city.jpg",
+                        "away" => "https://apiv3.apifootball.com/badges/3086_brentford.jpg",
+                        "prediction" => "1",
+                        "status" => "completed",
+                        "home_score" => "2",
+                        "away_score" => "1",
+                    ],
+                    [
+                        "home" => "https://apiv3.apifootball.com/badges/3071_afc-bournemouth.jpg",
+                        "away" => "https://apiv3.apifootball.com/badges/88_chelsea.jpg",
+                        "prediction" => "2",
+                        "status" => "completed",
+                        "home_score" => "0",
+                        "away_score" => "1",
+                    ],
+                    [
+                        "home" => "https://apiv3.apifootball.com/badges/164_tottenham-hotspur.jpg",
+                        "away" => "https://apiv3.apifootball.com/badges/141_arsenal.jpg",
+                        "prediction" => "2",
+                        "status" => "completed",
+                        "home_score" => "0",
+                        "away_score" => "1",
+                    ],
+                    [
+                        "home" => "https://apiv3.apifootball.com/badges/3077_wolverhampton-wanderers.jpg",
+                        "away" => "https://apiv3.apifootball.com/badges/3100_newcastle-united.jpg",
+                        "prediction" => "2",
+                        "status" => "completed",
+                        "home_score" => "1",
+                        "away_score" => "2",
+                    ],
+                ];
+                break;
+        }
+
+        return [];
     }
 }
--- resources/js/Pages/Marketing/ListDesign.vue
+++ resources/js/Pages/Marketing/ListDesign.vue
@@ -1,57 +1,65 @@
 <script setup>
-
+const props = defineProps(['events'])
 </script>
 
 <template>
     <div class="pt-[20px] h-[100vh] banner_background">
-        <ul class="mx-auto w-fit flex justify-between text-white mb-[20px]">
-            <li class="h-[100%] w-[30%]  flex items-center justify-between">
-                <div class="w-[60px] h-[60px] mr-[20px] ">
-                    <img width="60" height="48" src="https://img.icons8.com/color/48/instagram-new--v1.png"
-                         alt="instagram-new--v1"/>
-                </div>
-                <p class="text-[20px]">@tipsmoto_</p>
+        <ul class="mx-auto w-full md:w-[500px] flex justify-around text-white mb-[20px] gap-x-2">
+            <li class="h-[100%] flex items-center justify-center">
+                <img width="30" height="40" src="https://img.icons8.com/color/48/instagram-new--v1.png"/>
+                <p class="text-[15px]">@tipsmoto_</p>
             </li>
-            <li class="h-[100%] w-[30%]  flex items-center justify-between">
-                <div class="w-[60px] h-[60px] mr-[20px] ">
-                    <img width="60" height="60" src="https://img.icons8.com/color/48/facebook-new.png"
-                         alt="facebook-new"/>
-                </div>
-                <p class="text-[20px]">@tipsmoto_</p>
+            <li class="h-[100%] flex items-center justify-center">
+                <img width="30" height="40" src="https://img.icons8.com/color/48/facebook-new.png"/>
+                <p class="text-[15px]">@tipsmoto_</p>
             </li>
-            <li class="h-[100%] w-[30%]  flex items-center justify-between">
-                <div class="w-[60px] h-[60px] mr-[20px] ">
-                    <img width="60" height="60" src="https://img.icons8.com/color/48/twitterx--v1.png"
-                         alt="twitterx--v1"/>
-                </div>
-                <p class="text-[20px]">@tipsmoto_</p>
+            <li class="h-[100%] flex items-center justify-center">
+                <img width="30" height="40" src="https://img.icons8.com/color/48/twitterx--v1.png"/>
+                <p class="text-[15px]">@tipsmoto_</p>
             </li>
         </ul>
-        <div class="mx-auto w-[100px]">
+        <div class="mx-auto w-[80px] mb-[10px]">
             <img src="/storage/System/Icons/logo-dark.png" alt="">
         </div>
-        <div class="mx-auto w-[300px] h-[60px] trap">
-            <div class="w-full h-full">
-
-            </div>
-            <section class="text-white top-[0px] " style="position:relative; top: -40px">
-                <h1 style="font-size: 25px" class="p-[0px] m-[0px] h-full text-center font-bold">PREMIER LEAGUE</h1>
+        <div class="mx-auto w-[150px] h-[40px] trap">
+            <div class="w-full h-full"></div>
+            <section class="text-white w-full relative px-[10px]" style="top: -30px">
+                <h1 class="text-center w-full text-[14px] font-bold">PREMIER LEAGUE</h1>
             </section>
         </div>
-        <div class="board pt-[10px]">
-            <h1 style="font-size: 20px; text-shadow: 1px 1px 2px black; " class="p-[0px] mb-[20px] m-[0px]  text-center font-bold text-white">THIS WEEKEND MOTO MOTO GAMES</h1>
-            <div class="heading-div">
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-            <ol style="list-style:numeric" class="w-[500px] text-white text-[20px] p-[20px] mx-auto">
-                <li v-for="item in 5">MANCHESTER UNITED VS CHELSEA - AWAY WIN</li>
-            </ol>
-            <div>
-                <p class="w-[500px] text-white text-[20px] p-[20px] mx-auto">MOTO MOTO LEVEL: 96% WIN RATE</p>
-            </div>
+        <div class="px-[20px] ">
+            <div class="board pt-[10px] mx-auto max-w-[400px] px-[20px]">
+                <h1 style="text-shadow: 1px 1px 2px black;"
+                    class="p-[0px] text-[18px] mb-[20px] m-[0px]  text-center text-white">MOTO MOTO GAMES</h1>
+                <ul class="text-white text-[14px] p-[1px] mx-auto mb-[10px]">
+                    <li v-for="item in events"
+                        class="px-[10px] bg-green-500 mb-[5px] rounded-xl flex items-start justify-between w-fit mx-auto ">
+                        <div class="flex items-center gap-x-2">
+                            <div class="w-[50px] h-[50px] rounded bg-white mt-[5px] p-[1px]">
+                                <img width="50px" height="50px" :src="item.home" alt="manchester-united-fc"/>
+                            </div>
+                            <p class="text-[20px]">{{ item.home_score }}</p>
+                        </div>
+                        <div class="flex w-full flex-col px-[10px] pt-[20px]">
+                            <p class="text-center mb-[10px]">-</p>
+                            <p v-if="item.prediction && item.prediction == 1" class="mx-[20px] text-center">Home Win</p>
+                            <p v-else-if="item.prediction && item.prediction == 2" class="mx-[20px] text-center">Away
+                                Win</p>
+                            <p v-if="item.prediction && item.prediction == 'X'" class="mx-[20px] text-center">Draw</p>
+                        </div>
+                        <div class="flex items-center gap-x-2">
+                            <p class="text-[20px]">{{ item.away_score }}</p>
+                            <div class="w-[50px] h-[50px] rounded mt-[5px] bg-white p-[1px]">
+                                <img width="50px" height="50px" :src="item.away" alt="chelsea-fc"/>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <div>
+                    <p class=" text-white text-[18px] p-[5px] mx-auto text-center">MOTO MOTO LEVEL: 96% WIN RATE</p>
+                </div>
 
+            </div>
         </div>
     </div>
 </template>
@@ -59,62 +67,60 @@
 <style scoped lang="scss">
 
 .banner_background {
+    width: 500px;
+    margin: 0px auto;
+    margin-top: 20px;
+    height: 800px;
+    overflow: hidden;
     background-image: url(/storage/System/field_background.jpg);
-    background-repeat: no-repeat;
-    background-position: center;
     background-size: cover;
+    background-repeat: no-repeat;
 }
 
 .trap {
     div {
-        transform: perspective(100px) rotateX(30deg);
-        background-image: linear-gradient(180deg, rgba(93,3,8,1) 0%, rgba(212,57,68,1) 100%);
+        margin: 0 auto;
+        transform: perspective(60px) rotateX(30deg);
+        background-image: linear-gradient(180deg, rgba(93, 3, 8, 1) 0%, rgba(212, 57, 68, 1) 100%);
         box-shadow: 0px 0px 10px 1px #e23434;
     }
 }
-.board{
-    height: 500px;
-    width: 600px;
-    margin: auto;
-    position:relative;
+
+.board {
+    position: relative;
+    padding: 10px;
     top: -2px;
-    background-image: linear-gradient(180deg, rgba(212,57,68,1) 0%, rgba(100, 2, 2, 0.3), rgba(26, 1, 1, 0.8) 100%);
+    background-image: linear-gradient(180deg, rgba(212, 57, 68, 1) 0%, rgba(100, 2, 2, 0.3), rgba(26, 1, 1, 0.8) 100%);
     box-shadow: 0px 0px 10px 1px #e23434;
     border-radius: 2px;
 
-    .heading-div{
+    .heading-div {
         height: 50px;
         overflow: hidden;
         position: relative;
-        div:nth-of-type(1){
-            width: 200px;
-            height: 50px;
-            background-image:linear-gradient(135deg, #ea5d5d,#ea5d5d,#ea5d5d, #1a0101 ) ;
+
+        div:nth-of-type(1) {
+            background-image: linear-gradient(135deg, #ea5d5d, #ea5d5d, #ea5d5d, #1a0101);
             position: absolute;
-            top: 0px;
+            top: 0;
             left: 20px;
             transform: skew(30deg);
             z-index: 100;
             box-shadow: 0 0 10px #2b2b2b;
-
         }
-        div:nth-of-type(2){
-            width: 200px;
-            height: 50px;
+
+        div:nth-of-type(2) {
             background-color: red;
             position: absolute;
-            top: 0px;
+            top: 0;
             right: 20px;
             transform: skew(-30deg);
-            background-image:linear-gradient(-135deg, #ea5d5d,#ea5d5d,#ea5d5d, #1a0101 ) ;
+            background-image: linear-gradient(-135deg, #ea5d5d, #ea5d5d, #ea5d5d, #1a0101);
             box-shadow: 0 0 10px #2b2b2b;
-
             z-index: 100;
         }
-        div:nth-of-type(3){
-            width: 90%;
-            height: 45px;
-            margin: 0px auto;
+
+        div:nth-of-type(3) {
             position: relative;
             top: 3px;
             box-shadow: inset 0 15px 20px #0c0c0c;
