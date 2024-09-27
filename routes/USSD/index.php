3a7d2db0cb9250e2a7777f56d1e17bfa7e5155b3
Peter Mwaura <kimmwaus@gmail.com>
2024-09-27 16:19:58 +0300

    setting up ussd

--- routes/USSD/index.php
+++ routes/USSD/index.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 
-Route::prefix('USSD')->group(function (){
-    Route::get('/', function (Request $request){
+Route::prefix('/USSD')->group(function (){
+    Route::post('/', function (Request $request){
         return "Thank you for using our service END";
     });
 });
