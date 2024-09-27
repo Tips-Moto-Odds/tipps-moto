047de3f6cd54cf195a8245a450bc2f5a19bf7b1f
Peter Mwaura <kimmwaus@gmail.com>
2024-09-27 16:43:24 +0300

    setting up ussd

--- routes/USSD/index.php
+++ routes/USSD/index.php
@@ -4,9 +4,13 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-
 Route::prefix('/USSD')->group(function () {
     Route::post('/', function (Request $request) {
-        return "Thank you for using our service END";
+        // Set header for content-type as text/plain
+        $responseText = "CON Welcome to TipsMoto! \n1. Football \n2. Cricket \n3. Basketball \n4. Hockey \n5. Exit";
+
+        return response($responseText)
+            ->header('Content-Type', 'text/plain');
     });
 });
+
