dd41527c4689081cfa2a7c8eeca68f31d999f03e
Peter Mwaura <kimmwaus@gmail.com>
2024-09-27 17:04:14 +0300

    setting up ussd

--- routes/USSD/index.php
+++ routes/USSD/index.php
@@ -7,7 +7,15 @@ use Illuminate\Http\Request;
 Route::prefix('/USSD')->group(function () {
     Route::post('/', function (Request $request) {
         // Set header for content-type as text/plain
-        $responseText = "Welcome to TipsMoto! \n1. Football \n2. Cricket \n3. Basketball \n4. Hockey \n5. Exit END";
+        $responseText = "END hi Welcome to! \n1. Football \n2. Cricket \n3. Basketball \n4. Hockey \n5. Exit";
+
+        return response($responseText)
+            ->header('Content-Type', 'text/plain');
+    });
+
+    Route::post('/Callback', function (Request $request) {
+        // Set header for content-type as text/plain
+        $responseText = "END Welcome to TipsMoto! \n1. Football \n2. Cricket \n3. Basketball \n4. Hockey \n5. Exit END";
 
         return response($responseText)
             ->header('Content-Type', 'text/plain');
