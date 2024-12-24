24aec95923989d14c4edcdb61bdaa29306874a05
Peter Mwaura <kimmwaus@gmail.com>
2024-12-24 17:04:40 +0300

    Social Link confirmed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,5 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolesSeeder::class);
         $this->call(AdminUserSeeder::class);
-//        $this->call(UserSeeder::class);
     }
 }
