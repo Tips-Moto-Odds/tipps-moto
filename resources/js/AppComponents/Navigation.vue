4d6ca59c6a01645121da22e4f1a1a2f399e48ed8
Peter Mwaura <kimmwaus@gmail.com>
2024-12-25 13:09:10 +0300

    Menu update

--- resources/js/AppComponents/Navigation.vue
+++ resources/js/AppComponents/Navigation.vue
@@ -1,28 +1,62 @@
 <script setup>
 import {usePage} from "@inertiajs/vue3";
+import {ref} from "vue";
+
 const page = usePage()
-</script>
-<template>
-  <div class="w-[99%] shadow flex justify-content-between align-items-sm-center mb-[20px] px-[20px] mx-auto rounded mt-[20px] bg-gray-700 h-[67px]">
-    <div class="text-white px-[10px] py-[3px] rounded ">
-        <img class="w-[60px] p-1" src="storage/System/Icons/logo-dark.png">
-    </div>
-    <ul class=" flex gap-xl-2 m-0">
-      <Link class="menu-button" :class="{'active':page.url == '/'}" href="/" as="li">Home</Link>
-      <Link class="menu-button" :class="{'active':page.url == '/tips'}" href="/tips" as="li">Tips</Link>
-      <Link class="menu-button" href="/contact" as="li">Contact</Link>
-      <Link class=" text-black align-items-sm-center  px-[10px] rounded" href="/dashboard" as="li">
-        <i class="bi bi-person-circle text-white text-[30px]"></i>
-      </Link>
-    </ul>
-  </div>
-</template>
-<style lang="scss" scoped>
-.menu-button{
-    @apply text-white self-center text-[16px]  px-3 py-2 rounded hover:bg-gray-500 cursor-pointer;
+
+const currentPage = () => {
+    switch (page.url) {
+        case "/":
+            return "Home"
+        case "/tips":
+            return "Tips"
+        case "/contact":
+            return "Tips"
+        default:
+            return "Tips Moto"
+    }
 }
 
-.menu-button.active{
-    @apply bg-orange-500;
+function dropDownMenu() {
+    if ($('#home-menu').height() > 100) {
+        $('#home-menu').css('height', '70px')
+    } else {
+        $('#home-menu').css('height', '340px')
+    }
+}
+</script>
+<template>
+    <div id="home-menu" class="flex justify-between mb-[20px] !mx-[10px] mt-[10px] px-[20px] rounded shadow bg-gray-700 h-[70px] transition duration-200 ease-in-out md:mt-[20px] overflow-hidden">
+        <div class="text-white  py-[3px] rounded ">
+            <img class="w-[60px] p-1" src="storage/System/Icons/logo-dark.png">
+        </div>
+        <ul class="p-[15px] gap-xl-2 m-0 w-[90%] flex flex-col md:w-fit md:flex-row md:pt-[40px] ">
+            <li class="menu-button md:hidden">{{currentPage()}}</li>
+            <Link class="menu-button" :class="{'active':page.url == '/'}" href="/" as="li">Home</Link>
+            <Link class="menu-button" :class="{'active':page.url == '/tips'}" href="/tips" as="li">Tips</Link>
+            <Link class="menu-button" :class="{'active':page.url == '/contact'}" href="/contact" as="li">Contact</Link>
+            <Link class="menu-button" :class="{'active':page.url == '/dashboard'}" href="/dashboard" as="li">Dashboard</Link>
+            <Link class=" text-black hidden md:block rounded menu-button" href="/dashboard" as="li">
+                <i class="bi bi-person-circle text-white text-[30px]"></i>
+            </Link>
+        </ul>
+        <div class="md:hidden pt-[5px]">
+            <i class="bi text-[40px] text-white bi-list" @click.prevent="dropDownMenu"></i>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+#home-menu {
+    transition: all ease 0.5s;
+    position: sticky !important;
+    top: 10px;
+}
+
+.menu-button {
+    @apply text-white self-center mb-[25px] text-[16px] w-[100px] md:w-fit px-3 py-2 rounded hover:bg-gray-500 cursor-pointer;
+}
+
+.menu-button.active {
+    @apply md:bg-orange-500;
 }
 </style>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -9,37 +9,37 @@ const props = defineProps(['tips'])
 
 <template>
     <Navigation/>
-    <div class="banner container flex justify-content-between mb-[20px] mx-auto rounded">
-        <div class=" h-[410px] w-[500px] grid place-items-center rounded overflow-hidden">
-            <img class="max-w-[100%] max-h-[100%]" src="/storage/System/content/banners/banner1.png">
-        </div>
-        <div class="w-[calc(100%-510px)] h-[400px] ">
-            <div class="h-[50%]  mb-[10px] flex gap-x-[10px]">
-                <div class="w-[50%] h-[100%] rounded bg-gray-400">
-                    <img src="storage">
-                </div>
-                <div class="w-[50%] flex flex-column relative justify-center items-center gap-0 h-[100%] rounded ">
-                    <img class="w-100 h-100 object-cover" src="/storage/System/content/banners/tips-banner.png">
-                    <div class="absolute top-0 left-0  z-2 w-100 h-100 flex justify-center items-center" style="background-color: rgba(0,0,0,.8)">
-                        <h2 class="text-center text-white">Get Daily Tips<br> for as low as<br> <span class="text-orange-500 text-[8s0px]">15/-</span></h2>
-                    </div>
-                </div>
-            </div>
-            <div class="w-[100%] h-[50%] rounded overflow-hidden">
-                <JackpotCarousel/>
-            </div>
-        </div>
-    </div>
+<!--    <div class="banner container flex justify-content-between mb-[20px] mx-auto rounded">-->
+<!--        <div class=" h-[410px] w-[500px] grid place-items-center rounded overflow-hidden">-->
+<!--            <img class="max-w-[100%] max-h-[100%]" src="/storage/System/content/banners/banner1.png">-->
+<!--        </div>-->
+<!--        <div class="w-[calc(100%-510px)] h-[400px] ">-->
+<!--            <div class="h-[50%]  mb-[10px] flex gap-x-[10px]">-->
+<!--                <div class="w-[50%] h-[100%] rounded bg-gray-400">-->
+<!--                    <img src="storage">-->
+<!--                </div>-->
+<!--                <div class="w-[50%] flex flex-column relative justify-center items-center gap-0 h-[100%] rounded ">-->
+<!--                    <img class="w-100 h-100 object-cover" src="/storage/System/content/banners/tips-banner.png">-->
+<!--                    <div class="absolute top-0 left-0  z-2 w-100 h-100 flex justify-center items-center" style="background-color: rgba(0,0,0,.8)">-->
+<!--                        <h2 class="text-center text-white">Get Daily Tips<br> for as low as<br> <span class="text-orange-500 text-[8s0px]">15/-</span></h2>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--            <div class="w-[100%] h-[50%] rounded overflow-hidden">-->
+<!--                <JackpotCarousel/>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
 
-    <div class="content-holder container">
-        <h1 class="mb-[40px] text-white">Today's Free tips</h1>
-        <ul class="mb-[20px]">
-            <HomeTipsDisplay v-if="tips && tips.length > 0" v-for="item in tips" :tip="item" :key="item.id"/>
-            <p v-else class="text-white h2 text-center bg-gray-600 rounded py-[100px]">No Free Tips Available</p>
-        </ul>
-        <button class="action-button text-white bg-gray-600 hover:bg-orange-500 rounded">More Tips</button>
-    </div>
-    <app-footer-main/>
+<!--    <div class="content-holder container">-->
+<!--        <h1 class="mb-[40px] text-white">Today's Free tips</h1>-->
+<!--        <ul class="mb-[20px]">-->
+<!--            <HomeTipsDisplay v-if="tips && tips.length > 0" v-for="item in tips" :tip="item" :key="item.id"/>-->
+<!--            <p v-else class="text-white h2 text-center bg-gray-600 rounded py-[100px]">No Free Tips Available</p>-->
+<!--        </ul>-->
+<!--        <button class="action-button text-white bg-gray-600 hover:bg-orange-500 rounded">More Tips</button>-->
+<!--    </div>-->
+<!--    <app-footer-main/>-->
 </template>
 
 <style lang="scss" scoped>
