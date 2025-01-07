5e73c0b18f58335974b870a8f86c96b60f07202c
Peter Mwaura <kimmwaus@gmail.com>
2025-01-07 14:54:02 +0300

    cookie consent

--- lang/vendor/cookie-consent/en/texts.php
+++ lang/vendor/cookie-consent/en/texts.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'message' => 'We use cookies to ensure the proper functioning of our website. By using our site, you consent to the use of these cookies.Please read our cookie policy to learn more.',
+    'message' => 'We use cookies to personalize your experience, for measurement and analytics purposes, and to show you ads based on your preferences. By using our website and services, you agree to our use of cookies as described in our Cookie Policy.',
     'agree' => 'Allow cookies',
 ];
