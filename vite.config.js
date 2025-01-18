d5fad285adb7e5f033b41927f59db9bdf0f9ecef
Peter Mwaura <kimmwaus@gmail.com>
2025-01-18 13:50:38 +0300

    update main

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,15 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/js/app.js',
-                'routes/**/*.php',  // This covers all PHP files in the routes directory
-                'app/Http/Controllers/**/*.php'  // This covers all controllers
-            ],
-            refresh: [
-                'routes/**/*.php',  // Watch for changes in routes files
-                'app/Http/Controllers/**/*.php'  // Watch for changes in controllers files
-            ],
+            // input: [
+            //     'resources/js/app.js',
+            //     'routes/**/*.php',  // This covers all PHP files in the routes directory
+            //     'app/Http/Controllers/**/*.php'  // This covers all controllers
+            // ],
+            // refresh: [
+            //     'routes/**/*.php',  // Watch for changes in routes files
+            //     'app/Http/Controllers/**/*.php'  // Watch for changes in controllers files
+            // ],
         }),
         vue({
             template: {
