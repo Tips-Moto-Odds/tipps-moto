965c090d0efd087d1385a55598dd34cd02d4a63d
Peter Mwaura <kimmwaus@gmail.com>
2025-02-03 10:00:04 +0300

    Fixed banner

--- resources/js/AppComponents/MainBanner.vue
+++ resources/js/AppComponents/MainBanner.vue
@@ -4,10 +4,10 @@ import JackpotCarousel from "@/AppComponents/JackpotCarousel.vue";
 
 <template>
   <div class="banner container p-0  flex justify-between flex-col md:flex-row mb-[20px] mx-auto rounded">
-    <div class="grid mr-[10px] place-items-center rounded overflow-hidden ratio-1x1 md:w-1/2">
+    <div class="grid  place-items-center rounded overflow-hidden ratio-1x1 md:w-1/2 mx-auto md:mr-[10px] w-[95%] mb-[10px] md:mb-0">
       <img src="/storage/System/content/banners/banner1.png" class="md:h-full object-cover">
     </div>
-    <div class="w-[100%] md:w-1/2 ">
+    <div class="w-[100%] md:w-1/2 px-[10px]">
       <div class="h-[50%] flex mb-[10px] gap-x-[10px]">
         <Link href="/tips#ftp" as="div" class="w-[50%] rounded overflow-hidden">
           <img class="h-100 object-fit-cover" src="/storage/System/content/banners/tips-banner.png">
