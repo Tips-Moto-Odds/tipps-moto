d02bf8d22c9e20bc423379f15935ec369eb68d88
Peter Mwaura <kimmwaus@gmail.com>
2025-02-04 18:08:39 +0300

    Redising home page, free tips, yesterdays section,purchase pop up,subscriptions page,tips, header footer

--- resources/js/AppComponents/HomeTipsDisplay.vue
+++ resources/js/AppComponents/HomeTipsDisplay.vue
@@ -12,13 +12,13 @@ const page = usePage();
     <div class="text-white  p-[10px] bg-app_gray w-100 md:max-w-[334px] rounded mb-[10px]">
         <section>
             <div class="flex justify-between mb-[10px]">
-                <p>1</p>
-<!--                <p>{{ useDateFormat(tip.match_start_time, 'ddd, DD MMM ') }}</p>-->
+                <p></p>
+                <p>{{ useDateFormat(tip.match_start_time, 'ddd, DD MMM ') }}</p>
                 <p></p>
             </div>
             <div class="flex justify-between items-center text-sm mb-[10px]">
                 <p class="w-100 text-right">{{ tip.home_teams }}</p>
-                <p class="mx-[2px] w-[200px] text-center">{{ useDateFormat(tip.match_start_time, 'HH:MM') }}</p>
+<!--                <p class="mx-[2px] w-[200px] text-center">{{ useDateFormat(tip.match_start_time, 'HH:MM') }}</p>-->
                 <p class="w-100 text-left">{{ tip.away_teams }}</p>
             </div>
             <div class="flex justify-center w-100 md:w-1/3">
