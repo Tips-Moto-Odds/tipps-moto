9ab00897725711739202ef099b72e7a59ff955c1
Peter Mwaura <kimmwaus@gmail.com>
2025-04-18 00:33:30 +0300

    Update Marketing Tab

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,13 @@
     use Illuminate\Http\Request;
     use Illuminate\Support\Facades\DB;
     use App\Jobs\SendPushNotifications;
+    use Illuminate\Support\Facades\Log;
     use Illuminate\Support\Facades\Route;
     use App\Http\Middleware\LogInAsAdmin;
     use App\Http\Middleware\CaptureAffiliateLink;
 
 
+    Log::info("test");
     Route::middleware([
                           CaptureAffiliateLink::class,
                           LogInAsAdmin::class
