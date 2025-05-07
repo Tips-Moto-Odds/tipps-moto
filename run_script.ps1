f0326b0f00cb09e0e2082a06ff73da9f6c6df6dd
Peter Mwaura <kimmwaus@gmail.com>
2025-05-07 12:22:51 +0300

    update to make tips free

--- run_script.ps1
+++ run_script.ps1
@@ -1,3 +1,3 @@
 # Run the Laravel development server
 Write-Host "Starting Laravel development server..."
-php artisan serve --host=127.0.0.1 --port=8000
+C:\wamp64\bin\php\php8.3.14\php.exe artisan serve --host=127.0.0.1 --port=8000
